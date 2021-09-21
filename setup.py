29e27129fdbcd0cb0eaacc3f6d7652fcff297d1b
Harri Mäkelin <hmakelin@protonmail.com>
2021-09-21 12:29:40 +0100

    Parse package.xml contents in setup.py to ensure there are no discrepancies between the two files.

--- setup.py
+++ setup.py
@@ -1,10 +1,19 @@
 from setuptools import setup
+import xml.etree.ElementTree as ET
 
-package_name = 'wms_map_matching'
+# Parse info from package.xml
+tree = ET.parse('package.xml')
+root = tree.getroot()
+package_name = root.find('name').text
+version = root.find('version').text
+description = root.find('description').text
+maintainer = root.find('maintainer').text
+maintainer_email = root.find('maintainer').attrib.get('email', '')
+license = root.find('license').text
 
 setup(
     name=package_name,
-    version='0.0.0',
+    version=version,
     packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
@@ -13,10 +22,10 @@ setup(
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='Harri Mäkelin',
-    maintainer_email='hmakelin@protonmail.com',
-    description='Matching of airborne drone video to map retrieved from OGC WMS endpoint.',
-    license='MIT',
+    maintainer=maintainer,
+    maintainer_email=maintainer_email,
+    description=description,
+    license=license,
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [

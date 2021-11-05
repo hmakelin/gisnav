1. Fix y-axis inversion in conver_pix_to_wgs84. 2. Add debugging visualization for uncropped, unrotatated FoV. 3. Changes map raster rotation in convert_fov_from_pix_to_wgs84 - this way the results are correct when testing in Gazebo/QGroundControl with camera tilt set to 70 degrees (slight perspective).

wms_map_matching/matching_node.py
@@ -21,7 +21,7 @@ from ament_index_python.packages import get_package_share_directory
 from wms_map_matching.util import get_bbox, setup_sys_path, convert_fov_from_pix_to_wgs84, \
     write_fov_and_camera_location_to_geojson, get_camera_apparent_altitude, get_camera_lat_lon, BBox,\
     Dimensions, get_camera_lat_lon_alt, MAP_RADIUS_METERS_DEFAULT, padded_map_size, rotate_and_crop_map,\
-    visualize_homography, process_matches, uncrop_pixel_coordinates, get_fov
+    visualize_homography, process_matches, uncrop_pixel_coordinates, get_fov, rotate_point
 
 # Add the share folder to Python path
 share_dir, superglue_dir = setup_sys_path()  # TODO: Define superglue_dir elsewhere? just use this to get share_dir
@@ -284,14 +284,20 @@ class Matcher(Node):
             for i in range(0, len(mkp_map)):
                 mkp_map_uncropped.append(list(uncrop_pixel_coordinates(self._img_dimensions(), self._map_dimensions_with_padding(), mkp_map[i])))
             mkp_map_uncropped = np.array(mkp_map_uncropped)
-            h2, h_mask2, translation_vector2, rotation_matrix2 = process_matches(mkp_img, mkp_map_uncropped,
+
+            mkp_map_unrotated = []
+            for i in range(0, len(mkp_map_uncropped)):
+                mkp_map_unrotated.append(list(rotate_point(rot, self._map_dimensions_with_padding(), mkp_map_uncropped[i])))
+            mkp_map_unrotated = np.array(mkp_map_unrotated)
+
+            h2, h_mask2, translation_vector2, rotation_matrix2 = process_matches(mkp_img, mkp_map_unrotated, # mkp_map_uncropped,
                                                                              self._camera_info().k.reshape([3, 3]),
                                                                              self._get_camera_normal(),
                                                                              logger=self._logger,
                                                                              affine=self._config['superglue']['misc']['affine'])
 
             fov_pix_2 = get_fov(self._cv_image, h2)
-            visualize_homography('Uncropped', self._cv_image, map_rotated, mkp_img, mkp_map_uncropped, fov_pix_2) # TODO: separate calculation of fov_pix from their visualization!
+            visualize_homography('Uncropped and unrotated', self._cv_image, self._map, mkp_img, mkp_map_unrotated, fov_pix_2) # TODO: separate calculation of fov_pix from their visualization!
             #### END ###
 
             camera_position = get_camera_lat_lon_alt(translation_vector, rotation_matrix, self._img_dimensions(),

wms_map_matching/util.py
@@ -146,7 +146,7 @@ def convert_fov_from_pix_to_wgs84(fov_in_pix, map_raster_padded_dim, map_raster_
     uncrop = partial(uncrop_pixel_coordinates, img_dim, map_raster_padded_dim)
     fov_in_pix_uncropped = np.apply_along_axis(uncrop, 2, fov_in_pix)
 
-    rotate = partial(rotate_point, -map_raster_rotation, map_raster_padded_dim)
+    rotate = partial(rotate_point, map_raster_rotation, map_raster_padded_dim)   # why not negative here?
     fov_in_pix_unrotated = np.apply_along_axis(rotate, 2, fov_in_pix_uncropped)
 
     convert = partial(convert_pix_to_wgs84, map_raster_padded_dim, map_raster_bbox)
@@ -170,7 +170,7 @@ def convert_pix_to_wgs84(img_dim, bbox, pt):
     """Converts a pixel inside an image to lat lon coordinates based on the image's bounding box.
 
     In cv2, y is 0 at top and increases downwards. x axis is 'normal' with x=0 at left."""
-    lat = bbox.bottom + (bbox.top - bbox.bottom) * pt[1] / img_dim.height  # TODO: use the 'LatLon' named tuple for pt
+    lat = bbox.bottom + (bbox.top - bbox.bottom) * (img_dim.height - pt[1]) / img_dim.height  # TODO: use the 'LatLon' named tuple for pt
     lon = bbox.left + (bbox.right - bbox.left) * pt[0] / img_dim.width
     return lat, lon
 
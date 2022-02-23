1b0323bda2cba8a3310b414d09a2cdca323b4613
Harri Mäkelin <hmakelin@protonmail.com>
2022-02-23 12:26:53 +0000

    Reorder map_nav_node imports to improve readability.

--- python_px4_ros2_map_nav/nodes/map_nav_node.py
+++ python_px4_ros2_map_nav/nodes/map_nav_node.py
@@ -28,10 +28,13 @@ from typing import Optional, Union, Tuple, get_args, List
 from rclpy.node import Node
 from rcl_interfaces.msg import ParameterDescriptor
 from geojson import Point, Polygon, Feature, FeatureCollection, dump
-
 from cv_bridge import CvBridge
 from scipy.spatial.transform import Rotation
 from functools import partial
+from px4_msgs.msg import VehicleAttitude, VehicleLocalPosition, VehicleGlobalPosition, GimbalDeviceAttitudeStatus, \
+    GimbalDeviceSetAttitude, VehicleGpsPosition
+from sensor_msgs.msg import CameraInfo, Image
+
 from python_px4_ros2_map_nav.data import BBox, Dim, LatLon, TimePair, RPY, LatLonAlt, ImageData, MapData
 from python_px4_ros2_map_nav.transform import fov_center, get_fov_and_c, pix_to_wgs84_affine, rotate_and_crop_map, \
     inv_homography_from_k_and_e, get_azimuth, axes_ned_to_image, make_keypoint
@@ -39,9 +42,6 @@ from python_px4_ros2_map_nav.assertions import assert_type, assert_ndim, assert_
 from python_px4_ros2_map_nav.ros_param_defaults import Defaults
 from python_px4_ros2_map_nav.keypoint_matchers.keypoint_matcher import KeypointMatcher
 from python_px4_ros2_map_nav.wms import WMSClient
-from px4_msgs.msg import VehicleAttitude, VehicleLocalPosition, VehicleGlobalPosition, GimbalDeviceAttitudeStatus, \
-    GimbalDeviceSetAttitude, VehicleGpsPosition
-from sensor_msgs.msg import CameraInfo, Image
 
 
 class MapNavNode(Node, ABC):

366d6f9424df5ce4da1c027fc83d1bc684bc0386
Harri Mäkelin <hmakelin@protonmail.com>
2022-05-13 07:39:12 +0100

    Reenable mock gps message publishing.

--- python_px4_ros2_map_nav/nodes/mock_gps_node.py
+++ python_px4_ros2_map_nav/nodes/mock_gps_node.py
@@ -113,7 +113,7 @@ class MockGPSNode(MapNavNode):
         msg.heading = np.nan
         msg.heading_offset = np.nan
         msg.selected = selection
-        #self._vehicle_gps_position_publisher.publish(msg)
+        self._vehicle_gps_position_publisher.publish(msg)
 
     def _export_position(self, position: Union[LatLon, LatLonAlt], fov: np.ndarray, filename: str) -> None:
         """Exports the computed position and field of view (FOV) into a geojson file.

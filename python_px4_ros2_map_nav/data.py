WIP: Rotation and altitude correct, translation scaling wrong.

python_px4_ros2_map_nav/data.py
@@ -290,8 +290,9 @@ class Match:
         index = 0  # Need to pick translation with positive z coordinate, negative x and y
         r, t = Rs[index], Ts[index]
         #t = self.image_pair.qry.fx*t  # scaling with depth
-        #t = np.multiply(t, match.camera_center)  # Scaling with world coordinates
-        t = np.multiply(t, match.camera_position)  # Scaling with world coordinates
+        t = np.multiply(t, match.camera_center)  # Scaling with world coordinates
+        #t = np.multiply(t, np.vstack((match.camera_position[0:2], match.camera_center[2])))  # Scaling with world coordinates
+        #t = 205*t
         t[2] = -t[2]
         print(f'scaled t {t}')
         print(f'camera center {match.camera_center} position {match.camera_position}')
@@ -299,7 +300,7 @@ class Match:
                 image_pair=ImagePair(qry=self.image_pair.qry, ref=match.image_pair.ref),
                 pose=Pose(
                     r,
-                    -(self.pose.r @ match.camera_center + t)
+                    -(r @ match.camera_center + t)
                 )  # TODO: need to fix sign somehow? Would think minus sign is needed here?
         )
 
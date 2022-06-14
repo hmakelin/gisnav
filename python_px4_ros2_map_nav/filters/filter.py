665d8c6efd7b276e529a30ed0e633d51905ba724
Harri Mäkelin <hmakelin@protonmail.com>
2022-06-14 09:57:30 +0100

    Remove commented out stuff and references to data.Position in filter.py.

--- python_px4_ros2_map_nav/filters/filter.py
+++ python_px4_ros2_map_nav/filters/filter.py
@@ -4,20 +4,9 @@ import numpy as np
 from abc import ABC, abstractmethod
 from typing import Optional, Tuple
 
-from python_px4_ros2_map_nav.nodes.data import Position
-
 
 class Filter(ABC):
     """Abstract base class for Kalman and Particle filters"""
-
-    #@abstractmethod
-    #def update(self, position: Position) -> Optional[Position]:
-    #    """Returns a filtered position with estimated standard deviations
-    #
-    #    :param position: A new position observation (measurement)
-    #    :return: Filtered position, AMSL altitude also gets filtered if provided, epx, epy and epz values provided of None if output not yet available
-    #    """
-    #    pass
 
     @abstractmethod
     def update(self, position: np.ndarray) -> Optional[Tuple[np.ndarray, np.ndarray]]:

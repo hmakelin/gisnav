5eb9e043abd93df6541e94466bcad449327ea7d2
Harri Mäkelin <hmakelin@protonmail.com>
2022-06-24 11:21:48 +0100

    Clean up MockGPSNode. Change QoS profile to SENSOR_DATA premade.

--- gisnav/nodes/mock_gps_node.py
+++ gisnav/nodes/mock_gps_node.py
@@ -7,6 +7,7 @@ from px4_msgs.msg import VehicleGpsPosition
 
 from gisnav.nodes.base_node import BaseNode
 from gisnav.data import Position
+from gisnav.assertions import assert_type
 
 
 class MockGPSNode(BaseNode):
@@ -14,67 +15,27 @@ class MockGPSNode(BaseNode):
 
     VEHICLE_GPS_POSITION_TOPIC_NAME = 'VehicleGpsPosition_PubSubTopic'
 
-    # ROS 2 QoS profiles for topics
-    # TODO: add duration to match publishing frequency, and publish every time (even if NaN)s.
-    # If publishing for some reason stops, it can be assumed that something has gone very wrong
-    PUBLISH_QOS_PROFILE = rclpy.qos.QoSProfile(history=rclpy.qos.HistoryPolicy.KEEP_LAST,
-                                               reliability=rclpy.qos.ReliabilityPolicy.RELIABLE,
-                                               depth=1)
-
     MISC_MOCK_GPS_SELECTION = 1
-    """GPS selection to include in mock GPS messages"""
+    """GPS selection parameter for outgoing :class:`px4_msgs.msg.VehicleGpsPosition` messages"""
 
     def __init__(self, name: str, package_share_dir: str):
-        """Class initializer"""
+        """Class initializer
+
+        :param name: Node name
+        :param package_share_dir: Package share directory
+        """
         super().__init__(name, package_share_dir)
         self._declare_ros_params()
-        self._vehicle_gps_position_publisher = self._create_publisher(self.VEHICLE_GPS_POSITION_TOPIC_NAME,
-                                                                      VehicleGpsPosition)
+        self._vehicle_gps_position_publisher = self.create_publisher(VehicleGpsPosition,
+                                                                     self.VEHICLE_GPS_POSITION_TOPIC_NAME,
+                                                                     rclpy.qos.QoSPresetProfiles.SENSOR_DATA.value)
 
     def publish(self, position: Position) -> None:
-        """Publishes position as :class:`px4_msgs.msg.VehicleGpsPosition message and as GeoJSON data"""
-        if not isinstance(position, Position) or not all([position.epv, position.eph]):
-            self.get_logger().warn('Some required fields required for publishing mock GPS message were None, '
-                                   'skipping publishing.')
-            return None
+        """Publishes position as :class:`px4_msgs.msg.VehicleGpsPosition` message"""
+        assert_type(position, Position)
 
         mock_gps_selection = self.get_parameter('misc.mock_gps_selection').get_parameter_value().integer_value
-        try:
-            self._publish_mock_gps_msg(position, mock_gps_selection)
-        except AssertionError as ae:
-            self.get_logger().error(f'Assertion error when trying to publish:\n{ae}')
 
-    def _declare_ros_params(self) -> None:
-        """Declares ROS parameters
-
-        :return:
-        """
-        try:
-            namespace = 'misc'
-            self.declare_parameters(namespace, [
-                ('mock_gps_selection', self.MISC_MOCK_GPS_SELECTION)
-            ])
-        except rclpy.exceptions.ParameterAlreadyDeclaredException as e:
-            self.get_logger().warn(str(e))
-
-    def _create_publisher(self, topic_name: str, class_: object) -> rclpy.publisher.Publisher:
-        """Sets up an rclpy publisher.
-
-        :param topic_name: Name of the microRTPS topic
-        :param class_: Message definition class (e.g. px4_msgs.msg.VehicleGpsPosition)
-        :return: The publisher instance
-        """
-        return self.create_publisher(class_, topic_name, self.PUBLISH_QOS_PROFILE)
-
-    # TODO: get camera_yaw/course estimate?
-    def _publish_mock_gps_msg(self, position: Position, selection: int) -> None:
-        """Publishes a mock :class:`px4_msgs.msg.VehicleGpsPosition` out of estimated position, velocities and errors.
-
-        :param position: Visually estimated vehicle position and attitude
-        :param selection: GPS selection (see :class:`px4_msgs.msg.VehicleGpsPosition` for comment)
-        :return:
-        """
-        assert all([position.eph, position.epv, position.z_amsl])
         msg = VehicleGpsPosition()
         msg.timestamp = position.timestamp
         msg.fix_type = 3
@@ -98,5 +59,15 @@ class MockGPSNode(BaseNode):
         msg.time_utc_usec = int(time.time() * 1e6)
         msg.heading = np.nan
         msg.heading_offset = np.nan
-        msg.selected = selection
+        msg.selected = mock_gps_selection
         self._vehicle_gps_position_publisher.publish(msg)
+
+    def _declare_ros_params(self) -> None:
+        """Declares ROS parameters"""
+        try:
+            namespace = 'misc'
+            self.declare_parameters(namespace, [
+                ('mock_gps_selection', self.MISC_MOCK_GPS_SELECTION)
+            ])
+        except rclpy.exceptions.ParameterAlreadyDeclaredException as e:
+            self.get_logger().warn(str(e))

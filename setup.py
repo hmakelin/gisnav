3c52d0eb65ca880d75be974e003590dd7a48f036
Harri Mäkelin <hmakelin@protonmail.com>
2022-08-23 18:38:58 +0100

    Declutter root folder by moving pose estimator submodules to a more appropriate place. Update setup.py, now takes into account that not all submodules may have been loaded. Should also now got through with the build_ros2_workspace.bash script, which uses --symlink-install. However, the pose estimators will not be found if --symlink-install is used so an additional colcon build --packages-select gisnav should be used when building.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from glob import glob
 from setuptools import setup
 
 from gisnav.data import PackageData
-package_data = PackageData.parse_package_data(os.path.abspath('package.xml'))
+pdata = PackageData.parse_package_data(os.path.abspath('package.xml'))
 
 # Read requirements file
 requirements_file = os.path.abspath('requirements.txt')
@@ -14,41 +14,57 @@ if os.path.isfile(requirements_file):
 else:
     raise FileNotFoundError(f'Could not find requirements file at {requirements_file}.')
 
-setup(
-    name=package_data.package_name,
-    version=package_data.version,
-    packages=[
-        package_data.package_name,
-        package_data.package_name + '.pose_estimators',
-        package_data.package_name + '.nodes',
+# Setup packages depending on what submodules have been downloaded
+packages_ = [
+    pdata.package_name,
+    pdata.package_name + '.pose_estimators',
+    pdata.package_name + '.nodes'
+]
+package_dir_ = {}
+package_data_ = {}
+
+if os.path.isdir('gisnav/pose_estimators/third_party/LoFTR'):
+    packages_.extend([
         'LoFTR.loftr',
         'LoFTR.loftr.backbone',
         'LoFTR.loftr.loftr_module',
         'LoFTR.loftr.utils',
+    ])
+    package_dir_.update({'LoFTR': 'gisnav/pose_estimators/third_party/LoFTR/src'})
+
+if os.path.isdir('gisnav/pose_estimators/third_party/SuperGluePretrainedNetwork'):
+    packages_.extend([
         'SuperGluePretrainedNetwork.models',
-    ],
-    package_dir={
-        'LoFTR': 'LoFTR/src',
-    },
-    package_data={
-        'SuperGluePretrainedNetwork.models': ['weights/*.pth'],
-    },
+    ])
+    package_data_.update(
+        {pdata.package_name + '.pose_estimators.third_party.SuperGluePretrainedNetwork.models':
+             ['weights/*.pth']
+         }
+    )
+    package_dir_.update({'SuperGluePretrainedNetwork': 'gisnav/pose_estimators/third_party/SuperGluePretrainedNetwork'})
+
+setup(
+    name=pdata.package_name,
+    version=pdata.version,
+    packages=packages_,
+    package_dir=package_dir_,
+    package_data=package_data_,
     data_files=[
-        ('share/ament_index/resource_index/packages', ['resource/' + package_data.package_name]),
-        ('share/' + package_data.package_name, ['package.xml']),
+        ('share/ament_index/resource_index/packages', ['resource/' + pdata.package_name]),
+        ('share/' + pdata.package_name, ['package.xml']),
         # Need to download weights separately, here in weights folder
-        (os.path.join('share', package_data.package_name, 'weights'), glob('weights/*.ckpt')),
-        (os.path.join('share', package_data.package_name, 'config'), glob('config/*.yaml')),
-        (os.path.join('share', package_data.package_name, 'launch'), glob('launch/*.launch*')),
+        (os.path.join('share', pdata.package_name, 'weights'), glob('weights/*.ckpt')),
+        (os.path.join('share', pdata.package_name, 'config'), glob('config/*.yaml')),
+        (os.path.join('share', pdata.package_name, 'launch'), glob('launch/*.launch*')),
     ],
     install_requires=install_requires,
     zip_safe=True,
-    author=package_data.author,
-    author_email=package_data.author_email,
-    maintainer=package_data.maintainer,
-    maintainer_email=package_data.maintainer_email,
-    description=package_data.description,
-    license=package_data.license_name,
+    author=pdata.author,
+    author_email=pdata.author_email,
+    maintainer=pdata.maintainer,
+    maintainer_email=pdata.maintainer_email,
+    description=pdata.description,
+    license=pdata.license_name,
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [

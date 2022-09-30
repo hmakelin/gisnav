1. Adds terrain and vehicle altitude above WGS 84 ellipsoid calculation (added attributes to InputData, Position and FixedCamera data classes, pygeodesy geoid instance to ArduPilotMAVROS). 2. Fixes attitude estimation (converts from MAVROS ENU to NED). 3. Remove debug logging message in mock GPS node. 4. Comment out use of VehicleGlobalPosition terrain_alt for vehicle altitude estimation for PX4 microRTPS bridge (use dist_bottom).

gisnav/autopilots/ardupilot_mavros.py
@@ -4,6 +4,7 @@ import time
 import numpy as np
 from typing import Optional, Callable, get_args
 
+from pygeodesy.geoids import GeoidPGM
 from scipy.spatial.transform import Rotation
 from sensor_msgs.msg import CameraInfo, Image
 from geometry_msgs.msg import PoseStamped
@@ -50,6 +51,9 @@ class ArduPilotMAVROS(Autopilot):
         :param image_callback: Callback function for camera image
         """
         super().__init__(node, camera_info_topic, image_topic, image_callback)
+
+        # TODO: copy to shared folder and use relative path
+        self._egm96 = GeoidPGM('/usr/share/GeographicLib/geoids/egm96-5.pgm', kind=-3)
 
         self._local_position_pose = None
         self._global_position_global = None
@@ -103,6 +107,27 @@ class ArduPilotMAVROS(Autopilot):
     def _time_sync(self, value: Optional[TimePair]) -> None:
         assert_type(value, get_args(Optional[TimePair]))
         self.__time_sync = value
+
+    @property
+    def _egm96(self) -> GeoidPGM:
+        """Geoid for converting MAVROS global position WGS 84 altitude to AMSL altitude
+
+        .. code-block: bash
+            :caption: geographiclib needed
+
+            sudo apt install geographiclib-tools
+            sudo geographiclib-get-geoids egm96-5
+
+        .. seealso::
+            `Avoiding Pitfalls Related to Ellipsoid Height and Height Above Mean Sea Level
+            <https://wiki.ros.org/mavros#mavros.2FPlugins.Avoiding_Pitfalls_Related_to_Ellipsoid_Height_and_Height_Above_Mean_Sea_Level>`_
+        """
+        return self.__egm96
+
+    @_egm96.setter
+    def _egm96(self, value: GeoidPGM) -> None:
+        assert_type(value, GeoidPGM)
+        self.__egm96 = value
     # endregion
 
     # region Private
@@ -199,7 +224,11 @@ class ArduPilotMAVROS(Autopilot):
 
     @property
     def attitude(self) -> Optional[Attitude]:
-        """Vehicle attitude in NED frame as an :class:`.Attitude` instance"""
+        """Vehicle attitude in NED frame as an :class:`.Attitude` instance
+
+        .. note::
+            MAVROS local position plugin returns orientation in ENU frame, it is converted to NED here
+        """
         if self._local_position_pose is not None \
                 and hasattr(self._local_position_pose, 'pose') \
                 and hasattr(self._local_position_pose.pose, 'orientation') \
@@ -207,10 +236,16 @@ class ArduPilotMAVROS(Autopilot):
                 and hasattr(self._local_position_pose.pose.orientation, 'y') \
                 and hasattr(self._local_position_pose.pose.orientation, 'z') \
                 and hasattr(self._local_position_pose.pose.orientation, 'w'):
-            return Attitude(np.array([self._local_position_pose.pose.orientation.x,
-                                      self._local_position_pose.pose.orientation.y,
-                                      self._local_position_pose.pose.orientation.z,
-                                      self._local_position_pose.pose.orientation.w]), extrinsic=True)
+
+            attitude_ned_q = np.array([self._local_position_pose.pose.orientation.y,
+                                       self._local_position_pose.pose.orientation.x,
+                                       -self._local_position_pose.pose.orientation.z,
+                                       self._local_position_pose.pose.orientation.w])
+
+            # Convert ENU->NED + re-center yaw
+            attitude_ned = Rotation.from_quat(attitude_ned_q) * Rotation.from_rotvec(np.array([0, 0, np.pi / 2]))
+
+            return Attitude(attitude_ned.as_quat(), extrinsic=True)
         else:
             return None
 
@@ -246,15 +281,19 @@ class ArduPilotMAVROS(Autopilot):
 
     @property
     def altitude_amsl(self) -> Optional[float]:
-        """Vehicle altitude in meters above mean sea level (AMSL) or None if not available
+        """Vehicle altitude in meters above mean sea level (AMSL) or None if not available"""
+        if self._global_position_global is not None and hasattr(self._global_position_global, 'altitude') and \
+                self.global_position is not None:
+            return self._global_position_global.altitude - self._egm96.height(self.global_position.lat,
+                                                                              self.global_position.lon)
+        else:
+            return None
 
-        .. note::
-            Assumes MAVROS global position altitude above ellipsoid :~ altitude AMSL
-
-        .. seealso::
-            :py:attr:`.altitude_amsl`
-        """
-        if self._global_position_global is not None and hasattr(self._global_position_global, 'altitude'):
+    @property
+    def altitude_ellipsoid(self) -> Optional[float]:
+        """Vehicle altitude in meters above WGS 84 ellipsoid or None if not available"""
+        if self._global_position_global is not None and hasattr(self._global_position_global, 'altitude') and \
+                self.global_position is not None:
             return self._global_position_global.altitude
         else:
             return None
@@ -272,6 +311,15 @@ class ArduPilotMAVROS(Autopilot):
         """
         if self.altitude_amsl is not None and self.altitude_agl is not None:
             return self.altitude_amsl - self.altitude_agl
+        else:
+            return None
+
+    # TODO: move to base class?
+    @property
+    def ground_elevation_ellipsoid(self) -> Optional[float]:
+        """Ground elevation in meters above WGS 84 ellipsoid or None if information is not available"""
+        if self.altitude_ellipsoid is not None and self.altitude_agl is not None:
+            return self.altitude_ellipsoid - self.altitude_agl
         else:
             return None
 

gisnav/data.py
@@ -55,7 +55,8 @@ class Position:
     z_ground: float             # altitude above ground plane in meters (positive)
     attitude: Attitude          # attitude in NED frame
     timestamp: int              # Reference timestamp of position
-    z_amsl: Optional[float]     # altitude above mean sea level (AMSL) in meters if known (positive)
+    z_amsl: Optional[float]     # altitude above mean sea level (AMSL) in meters if known
+    z_ellipsoid: Optional[float]# altitude above WGS84 ellipsoid in meters if known
 
     _KALMAN_FILTER_EPSG_CODE = 'epsg:3857'
     """Used for converting into an array that can be passed to :class:`.SimpleFilter"""
@@ -69,6 +70,10 @@ class Position:
 
         if self.z_ground < 0:
             raise DataValueError(f'Ground altitude was negative {self.z_ground}.')
+
+        if self.z_amsl is not None and self.z_ellipsoid is None or \
+                self.z_ellipsoid is not None and self.z_amsl is None:
+            raise DataValueError(f'Please provide both z_amsl and z_ellipsoid.')
 
     @property
     def lat(self) -> float:
@@ -374,8 +379,19 @@ class InputData:
     """Camera expected attitude"""
 
     ground_elevation: Optional[float]
-    """Assumed elevation of ground plane above mean sea level (AMSL)"""
+    """Assumed elevation of ground plane above mean sea level (AMSL) in meters"""
 
+    ground_elevation_ellipsoid: Optional[float]
+    """Assumed elevation of ground plane above WGS 84 ellipsoid in meters"""
+
+    def __post_init__(self):
+        """Post-initialization computed fields and validity checks
+
+        :raise: DataValueError if a valid data class could not be initialized
+        """
+        if self.ground_elevation is not None and self.ground_elevation_ellipsoid is None or \
+            self.ground_elevation_ellipsoid is not None and self.ground_elevation is None:
+                raise DataValueError('Please provide ground elevation both AMSL and above WGS 84 ellipsoid.')
 
 # noinspection PyClassHasNoInit
 @dataclass
@@ -428,7 +444,8 @@ class FixedCamera:
     image_pair: ImagePair
     pose: Pose
     timestamp: int
-    ground_elevation: Optional[float]
+    ground_elevation: Optional[float]  # Terrain altitude AMSL in meters
+    ground_elevation_ellipsoid: Optional[float]  # Terrain altitude above WGS 84 ellipsoid in meters
     fov: FOV = field(init=False)
     position: Position = field(init=False)
     h: np.ndarray = field(init=False)
@@ -512,11 +529,13 @@ class FixedCamera:
 
         return dst_fov, principal_point_dst
 
-    def _estimate_position(self, ground_elevation: Optional[float], crs: str = 'epsg:4326') -> Optional[Position]:
+    def _estimate_position(self, ground_elevation: Optional[float], ground_elevation_ellipsoid: Optional[float],
+                           crs: str = 'epsg:4326') -> Optional[Position]:
         """Estimates camera position (WGS84 coordinates + altitude in meters above mean sea level (AMSL)) as well as
         terrain altitude in meters.
 
-        :param ground_elevation: Optional ground elevation (needed to estimate altitude from sea level)
+        :param ground_elevation: Optional ground elevation above AMSL in meters
+        :param ground_elevation_ellipsoid: Optional ground elevation above WGS 84 ellipsoid in meters
         :param crs: CRS to use for the Position
         :return: Camera position
         """
@@ -538,6 +557,7 @@ class FixedCamera:
             xy=GeoPoint(lon, lat, crs),  # lon-lat order
             z_ground=alt,
             z_amsl=alt + ground_elevation if ground_elevation is not None else None,
+            z_ellipsoid=alt + ground_elevation_ellipsoid if ground_elevation_ellipsoid is not None else None,
             attitude=self._estimate_attitude(),
             timestamp=self.image_pair.qry.timestamp
         )
@@ -550,6 +570,10 @@ class FixedCamera:
         :raise: DataValueError if a valid FixedCamera could not be initialized
         """
         img = self.image_pair.qry
+        if self.ground_elevation is not None and self.ground_elevation_ellipsoid is None or \
+            self.ground_elevation_ellipsoid is not None and self.ground_elevation is None:
+                raise DataValueError('Please provide ground elevation both AMSL and above WGS 84 ellipsoid.')
+
         object.__setattr__(self, 'h', img.camera_data.k @ np.delete(self.pose.e, 2, 1))  # Remove z-column, making the matrix square
         try:
             object.__setattr__(self, 'inv_h', np.linalg.inv(self.h))
@@ -564,7 +588,7 @@ class FixedCamera:
             raise DataValueError('Could not initialize a valid FixedCamera.')
 
         try:
-            position = self._estimate_position(self.ground_elevation)
+            position = self._estimate_position(self.ground_elevation, self.ground_elevation_ellipsoid)
             if position is not None:
                 object.__setattr__(self, 'position', position)
             else:

gisnav/nodes/mock_gps_node.py
@@ -51,7 +51,6 @@ class MockGPSNode(BaseNode):
         """
         assert_type(fixed_camera, FixedCamera)
         msg = self._generate_sensor_gps(fixed_camera) if self._px4_micrortps else self._generate_gps_input(fixed_camera)
-        self.get_logger().error(f'publishing {msg}')
         self._gps_publisher.publish(msg)
 
     def _generate_gps_input(self, fixed_camera: FixedCamera) -> GPSINPUT:
@@ -69,14 +68,14 @@ class MockGPSNode(BaseNode):
 
         msg = GPSINPUT()
         msg.header = header
-        msg.gps_id = 1
+        msg.gps_id = 0
         msg.ignore_flags = 0
         msg.time_week = gps_time.week_number
         msg.time_week_ms = int(gps_time.time_of_week * 1e3)
         msg.fix_type = 3  # 3D position
         msg.lat = int(position.lat * 1e7)
         msg.lon = int(position.lon * 1e7)
-        msg.alt = float(position.z_amsl * 1e3)
+        msg.alt = float(position.z_ellipsoid)
         msg.horiz_accuracy = 10.0  # position.eph
         msg.vert_accuracy = 3.0  # position.epv
         msg.speed_accuracy = np.nan
@@ -86,7 +85,7 @@ class MockGPSNode(BaseNode):
         msg.ve = np.nan
         msg.vd = np.nan
         msg.satellites_visible = np.iinfo(np.uint8).max
-        msg.yaw = int(np.degrees(position.attitude.yaw) * 100)
+        msg.yaw = int(np.degrees(position.attitude.yaw % (2 * np.pi)) * 100)
 
         return msg
 
@@ -106,7 +105,7 @@ class MockGPSNode(BaseNode):
         msg.lat = int(position.lat * 1e7)
         msg.lon = int(position.lon * 1e7)
         msg.alt = int(position.z_amsl * 1e3)
-        msg.alt_ellipsoid = msg.alt
+        msg.alt_ellipsoid = int(position.z_ellipsoid * 1e3)
         msg.eph = 10.0  # position.eph
         msg.epv = 3.0  # position.epv
         msg.hdop = 0.0
1209e057cb63558ac803ef24c084ad67b4dbe666
Harri Mäkelin <hmakelin@protonmail.com>
2023-01-12 18:40:44 +0000

    fix(map_node): Add missing max_map_radius ROS param default value and declaration [skip ci]

--- gisnav/nodes/map_node.py
+++ gisnav/nodes/map_node.py
@@ -104,6 +104,9 @@ class MapNode(CameraSubscriberNode):
     ROS_D_MAP_OVERLAP_UPDATE_THRESHOLD = 0.85
     """Overlap ration between FOV and current map, under which a new map will be requested."""
 
+    ROS_D_MAX_MAP_RADIUS = 1000
+    """Max radius for circle inside the maps (half map side length)"""
+
     ROS_D_MAP_UPDATE_UPDATE_DELAY = 1
     """Default delay in seconds for throttling WMS GetMap requests
     
@@ -139,6 +142,7 @@ class MapNode(CameraSubscriberNode):
         ('transparency', ROS_D_IMAGE_TRANSPARENCY, False),
         ('format', ROS_D_IMAGE_FORMAT, False),
         ('map_overlap_update_threshold', ROS_D_MAP_OVERLAP_UPDATE_THRESHOLD, False),
+        ('max_map_radius', ROS_D_MAX_MAP_RADIUS, False),
     ]
     """List containing ROS parameter name, default value and read_only flag tuples"""
 

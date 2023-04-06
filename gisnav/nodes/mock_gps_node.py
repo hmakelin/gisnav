6c66b80ea848153a8bb0e37713662598bf32e693
Harri Mäkelin <hmakelin@protonmail.com>
2023-04-06 20:44:57 +0100

    Remove dead code from mock GPS node

--- gisnav/nodes/mock_gps_node.py
+++ gisnav/nodes/mock_gps_node.py
@@ -1,12 +1,10 @@
 """Publishes mock GPS (GNSS) messages"""
 from datetime import datetime
-from typing import Generic, TypeVar
 
 import numpy as np
 from geographic_msgs.msg import GeoPoseStamped
 from gps_time import GPSTime
 from mavros_msgs.msg import GPSINPUT, Altitude
-from px4_msgs.msg import SensorGps
 from rclpy.qos import QoSPresetProfiles
 
 from gisnav.data import Attitude
@@ -14,24 +12,9 @@ from gisnav.nodes.base.base_node import BaseNode
 
 from . import messaging
 
-_MsgType = TypeVar("_MsgType", SensorGps, dict)
-"""Type variable for outgoing GPS messages"""
 
-
-class MockGPSNode(BaseNode, Generic[_MsgType]):
+class MockGPSNode(BaseNode):
     """A node that publishes a mock GPS message over the microRTPS bridge"""
-
-    ROS_D_UDP_IP = "127.0.0.1"
-    """MAVProxy GPSInput plugin default host"""
-
-    ROS_D_UDP_PORT = 25100
-    """MAVProxy GPSInput plugin default port"""
-
-    ROS_PARAM_DEFAULTS = [
-        ("udp_ip", ROS_D_UDP_IP, True),
-        ("udp_port", ROS_D_UDP_PORT, True),
-    ]
-    """List containing ROS parameter name, default value and read_only flag tuples"""
 
     def __init__(self, name: str):
         """Class initializer
@@ -40,18 +23,9 @@ class MockGPSNode(BaseNode, Generic[_MsgType]):
         """
         super().__init__(name)
 
-        self._udp_ip = self.get_parameter("udp_ip").get_parameter_value().string_value
-        self._udp_port = (
-            self.get_parameter("udp_port").get_parameter_value().integer_value
-        )
-
-        # TODO: try to get MAVROS to work for GPSINPUT message and get rid
-        #  of UDP socket
         self._mock_gps_pub = self.create_publisher(
             GPSINPUT, messaging.ROS_TOPIC_GPS_INPUT, QoSPresetProfiles.SENSOR_DATA.value
         )
-        # self._socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
         self._vehicle_geopose_estimate_sub = self.create_subscription(
             GeoPoseStamped,
             messaging.ROS_TOPIC_VEHICLE_GEOPOSE_ESTIMATE,
@@ -110,11 +84,6 @@ class MockGPSNode(BaseNode, Generic[_MsgType]):
         msg = self._generate_gps_input(lat, lon, altitude_amsl, heading, timestamp)
 
         if msg is not None:
-            # assert_type(msg, dict)
-            # assert self._socket is not None
-            # self._socket.sendto(
-            #    f"{json.dumps(msg)}".encode("utf-8"), (self._udp_ip, self._udp_port)
-            # )
             self._mock_gps_pub.publish(msg)
         else:
             self.get_logger().info("Could not create GPS message, skipping publishing.")
@@ -166,11 +135,3 @@ class MockGPSNode(BaseNode, Generic[_MsgType]):
         )
 
         return msg
-
-    def destroy_node(self) -> None:
-        """Closes GPS_INPUT MAVLink UDP socket when node is destroyed"""
-        # self.get_logger().info("Closing UDP socket...")
-        # self._socket.close()
-
-        # socket closed - call parent method
-        super().destroy_node()

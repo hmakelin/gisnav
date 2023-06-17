14e173e3340d5c06d8d96a0728886538d5cef3b8
Harri Mäkelin <hmakelin@protonmail.com>
2023-06-17 10:00:58 +0100

    Fix relative topics (temporary, going to migrate to static_configuration.py)

--- gisnav/gisnav/messaging.py
+++ gisnav/gisnav/messaging.py
@@ -13,37 +13,37 @@ from ._data import BBox
 from ._geo import GeoPt
 
 # region ROS topic names
-ROS_TOPIC_BOUNDING_BOX = "gisnav/bounding_box"
+ROS_TOPIC_BOUNDING_BOX = "/gisnav/bounding_box"
 """Name of ROS topics into which :class:`geographic_msgs.msg.BoundingBox`
 will be published"""
 
-ROS_TOPIC_VEHICLE_GEOPOSE = "gisnav/vehicle_geopose"
+ROS_TOPIC_VEHICLE_GEOPOSE = "/gisnav/vehicle_geopose"
 """Name of ROS topic into which :class:`geographic_msgs.msg.GeoPoseStamped`
 will be published"""
 
-ROS_TOPIC_VEHICLE_GEOPOSE_ESTIMATE = "gisnav/vehicle_geopose/estimate"
+ROS_TOPIC_VEHICLE_GEOPOSE_ESTIMATE = "/gisnav/vehicle_geopose/estimate"
 """Name of ROS topic into which :class:`geographic_msgs.msg.GeoPoseStamped`
 estimate will be published"""
 
-ROS_TOPIC_VEHICLE_ALTITUDE = "gisnav/vehicle_altitude"
+ROS_TOPIC_VEHICLE_ALTITUDE = "/gisnav/vehicle_altitude"
 """Name of ROS topics into which :class:`mavros_msgs.msg.Altitude` will be published"""
 
-ROS_TOPIC_VEHICLE_ALTITUDE_ESTIMATE = "gisnav/vehicle_altitude/estimate"
+ROS_TOPIC_VEHICLE_ALTITUDE_ESTIMATE = "/gisnav/vehicle_altitude/estimate"
 """Name of ROS topics into which :class:`mavros_msgs.msg.Altitude` estimate
 will be published"""
 
-ROS_TOPIC_GIMBAL_QUATERNION = "gisnav/gimbal_quaternion"
+ROS_TOPIC_GIMBAL_QUATERNION = "/gisnav/gimbal_quaternion"
 """Name of ROS topics into which :class:`geometry_msgs.msg.Quaternion` will
 be published"""
 
-ROS_TOPIC_HOME_GEOPOINT = "gisnav/home_geopoint"
+ROS_TOPIC_HOME_GEOPOINT = "/gisnav/home_geopoint"
 """Name of ROS topics into which :class:`geographic_msgs.msg.GeoPointStamped`
 will be published"""
 
-ROS_TOPIC_ORTHOIMAGE = "gisnav/orthoimage_3d"
+ROS_TOPIC_ORTHOIMAGE = "/gisnav/orthoimage_3d"
 """ROS publish topic for :class:`.OrthoImage3D` message"""
 
-ROS_TOPIC_EGM96_HEIGHT = "gisnav/egm96_height"
+ROS_TOPIC_EGM96_HEIGHT = "/gisnav/egm96_height"
 """ROS publish topic for EGM96 ellipsoid height :class:`std_msgs.msg.Float32`
 message"""
 
@@ -59,10 +59,10 @@ ROS_TOPIC_SENSOR_GPS = "/fmu/in/sensor_gps"
 """Name of ROS topic for outgoing :class:`px4_msgs.msg.SensorGps` messages
 over PX4 DDS bridge"""
 
-ROS_TOPIC_CAMERA_INFO = "camera/camera_info"
+ROS_TOPIC_CAMERA_INFO = "/camera/camera_info"
 """Name of ROS topic for :class:`sensor_msgs.msg.CameraInfo` messages"""
 
-ROS_TOPIC_IMAGE = "camera/image_raw"
+ROS_TOPIC_IMAGE = "/camera/image_raw"
 """Name of ROS topic for :class:`sensor_msgs.msg.Image` messages"""
 
 ROS_TOPIC_HOME_POSITION = "/mavros/home_position/home"

8390c9fbcecd6fb8636b69eb60295125ddd7b68a
Harri Mäkelin <hmakelin@protonmail.com>
2023-07-01 11:39:21 +0100

    Add bounce effect on scrolling to top

--- docs/_static/js/custom.js
+++ docs/_static/js/custom.js
@@ -8,4 +8,23 @@ window.onscroll = function() {
     header.classList.add('bd-header-shadow');
     header.classList.remove('bd-header-no-shadow');
   }
+
+  if (window.pageYOffset === 0) {
+    document.body.classList.add('bounce');
+
+    // Get elements with class 'skip-link' and hide them
+    var skipLinks = document.getElementsByClassName('skip-link');
+    for (var i = 0; i < skipLinks.length; i++) {
+      skipLinks[i].style.display = 'none';
+    }
+
+    setTimeout(function() {
+      document.body.classList.remove('bounce');
+
+      // Show the 'skip-link' elements again
+      for (var i = 0; i < skipLinks.length; i++) {
+        skipLinks[i].style.display = 'block';
+      }
+    }, 500);
+  }
 };

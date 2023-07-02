55ac341090ea4b623e99d338c39eca9dc8a3fd13
Harri Mäkelin <hmakelin@protonmail.com>
2023-07-02 21:33:56 +0100

    Handle dark and light mode properly for mermaid diagrams [skip ci]

--- docs/_static/js/custom.js
+++ docs/_static/js/custom.js
@@ -28,3 +28,27 @@ window.onscroll = function() {
     }, 500);
   }
 };
+
+// Handle dark mode mermaid diagrams
+var observer = new MutationObserver(function(mutations) {
+  const dark = document.documentElement.dataset.theme == 'dark';
+  var mermaidTheme = dark ? 'dark' : 'default';
+  mermaid.initialize({startOnLoad:true, theme: mermaidTheme});
+
+  // Select all Mermaid divs
+  var mermaidDivs = document.querySelectorAll('.mermaid');
+
+  // Loop through each Mermaid div
+  mermaidDivs.forEach(function(div) {
+    // Retrieve the original Mermaid code from the 'data-mermaid' attribute
+    var mermaidCode = div.getAttribute('data-mermaid');
+
+    // Update the div's code, remove the 'data-processed' attribute
+    div.innerHTML = mermaidCode;
+    div.removeAttribute('data-processed');
+
+    // Re-render the Mermaid diagram
+    mermaid.init(undefined, div);
+  });
+});
+observer.observe(document.documentElement, {attributes: true, attributeFilter: ['data-theme']});

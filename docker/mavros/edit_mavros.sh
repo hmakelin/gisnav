5ae5e033bfe42927fb3ff58964f76d4d9e74476a
Harri Mäkelin <hmakelin@protonmail.com>
2023-07-29 10:05:13 +0100

    Improve edit_mavros script

--- docker/mavros/edit_mavros.sh
+++ docker/mavros/edit_mavros.sh
@@ -1,39 +1,26 @@
 #!/bin/bash
 
-# MAVROS extras and plugins
-# This script is brittle (TODO: fix mavros version/commit) but should make our
-# image leaner and quicker to build
+# Define the plugins, extras, messages and services we want to enable
+ENABLED_PLUGINS=("command" "param" "sys_status" "sys_time" "manual_control" "local_position" "global_position" "home_position" "altitude")
+ENABLED_EXTRAS=("gps_input" "hil" "gimbal_control")
+ENABLED_MSGS=("State" "FileEntry" "Waypoint" "EstimatorStatus" "ExtendedState" "StatusText" "VehicleInfo" "TimesyncStatus" "Param" "ParamEvent" "ParamValue" "ManualControl" "Mavlink" "RCOut" "GimbalDeviceInformation" "GimbalManagerSetPitchyaw" "GimbalManagerStatus" "GimbalManagerInformation" "HilControls" "HilActuatorControls" "HilStateQuaternion" "HilGPS" "HilSensor" "OpticalFlowRad" "RCIn" "GimbalDeviceAttitudeStatus" "GimbalManagerSetAttitude" "GimbalDeviceSetAttitude" "Altitude" "HomePosition" "GPSINPUT")
+ENABLED_SRVS=("ParamGet" "ParamPull" "ParamPush" "ParamSet" "ParamSetV2" "StreamRate" "VehicleInfoGet" "MessageInterval" "SetMode" "CommandLong" "CommandAck" "CommandInt" "CommandBool" "CommandHome" "CommandTOL" "CommandTriggerControl" "CommandTriggerInterval" "CommandVtolTransition" "EndpointAdd" "EndpointDel" "GimbalGetInformation" "GimbalManagerCameraTrack" "GimbalManagerConfigure" "GimbalManagerPitchyaw" "GimbalManagerSetRoi")
 
-# List required mavros plugins, extras, messages and services
-ENABLED_PLUGINS="command param sys_status sys_time manual_control local_position global_position home_position altitude"
-ENABLED_EXTRAS="gps_input hil gimbal_control"
-ENABLED_MSGS="State EstimatorStatus ExtendedState StatusText VehicleInfo TimesyncStatus Param ParamEvent ParamValue ManualControl Mavlink RCOut JointState GimbalDeviceInformation GimbalManagerSetPitchyaw GimbalManagerStatus GimbalManagerInformation HilControls HilActuatorControls HilStateQuaternion HilGPS HilSensor OpticalFlowRad RCIn GimbalDeviceAttitudeStatus GimbalManagerSetAttitude GimbalDeviceSetAttitude Altitude HomePosition HilGPS GPSINPUT"
-ENABLED_SRVS="ParamGet ParamPull ParamPush ParamSet ParamSetV2 StreamRate VehicleInfoGet MessageInterval SetMode CommandLong CommandAck CommandInt CommandBool CommandHome CommandTOL CommandTriggerControl CommandTriggerInterval CommandVtolTransition EndpointAdd EndpointDel GimbalGetInformation GimbalManagerCameraTrack GimbalManagerConfigure GimbalManagerPitchyaw GimbalManagerSetRoi"
+# Path to mavros source in colcon workspace
+MAVROS_PATH="src/mavros"
 
-# All available mavros plugins, extras, messages and services based on CMakeLists.txt
-ALL_PLUGINS="actuator_control altitude command dummy ftp geofence global_position home_position imu local_position manual_control mission_protocol_base nav_controller_output param rallypoint rc_io setpoint_accel setpoint_attitude setpoint_position setpoint_raw setpoint_trajectory setpoint_velocity sys_status sys_time waypoint wind_estimation"
-ALL_EXTRAS="3dr_radio adsb cam_imu_sync camera cellular_status companion_process_status debug_value distance_sensor esc_status esc_telemetry fake_gps gps_input gps_rtk gps_status guided_target hil landing_target log_transfer mag_calibration_status mocap_pose_estimate mount_control obstacle_distance odom onboard_computer_status optical_flow play_tune px4flow rangefinder terrain trajectory tunnel vfr_hud vibration vision_pose_estimate vision_speed_estimate wheel_odometry"
-ALL_MSGS="ADSBVehicle ActuatorControl Altitude AttitudeTarget CamIMUStamp CameraImageCaptured CellularStatus CommandCode CompanionProcessStatus DebugValue ESCInfo ESCInfoItem ESCStatus ESCStatusItem ESCTelemetry ESCTelemetryItem EstimatorStatus ExtendedState FileEntry GimbalDeviceAttitudeStatus GimbalDeviceInformation GimbalDeviceSetAttitude GimbalManagerInformation GimbalManagerSetAttitude GimbalManagerSetPitchyaw GimbalManagerStatus GPSINPUT GPSRAW GPSRTK GlobalPositionTarget HilActuatorControls HilControls HilGPS HilSensor HilStateQuaternion HomePosition LandingTarget LogData LogEntry MagnetometerReporter ManualControl Mavlink MountControl NavControllerOutput OnboardComputerStatus OpticalFlowRad OverrideRCIn Param ParamEvent ParamValue PlayTuneV2 PositionTarget RCIn RCOut RTCM RTKBaseline RadioStatus State StatusText TerrainReport Thrust TimesyncStatus Trajectory Tunnel VehicleInfo VfrHud Vibration Waypoint WaypointList WaypointReached WheelOdomStamped"
-ALL_SRVS="CommandAck CommandBool CommandHome CommandInt CommandLong CommandTOL CommandTriggerControl CommandTriggerInterval CommandVtolTransition EndpointAdd EndpointDel FileChecksum FileClose FileList FileMakeDir FileOpen FileRead FileRemove FileRemoveDir FileRename FileTruncate FileWrite GimbalGetInformation GimbalManagerCameraTrack GimbalManagerConfigure GimbalManagerPitchyaw GimbalManagerSetRoi LogRequestData LogRequestEnd LogRequestList MessageInterval MountConfigure ParamGet ParamPull ParamPush ParamSet ParamSetV2 SetMavFrame SetMode StreamRate VehicleInfoGet WaypointClear WaypointPull WaypointPush WaypointSetCurrent"
+# Function to build new list from enabled elements
+function build_new_list() {
+  local -n list=$1
+  local new_list=""
+  for element in ${list[@]}; do
+    new_list+="  $2/${element}.$3\n"
+  done
+  echo "$new_list"
+}
 
-# This script comments out the lines corresponding to the disabled plugins and extras in CMakeLists.txt
-for plugin in $ALL_PLUGINS; do \
-if [[ $ENABLED_PLUGINS != *"$plugin"* ]]; then \
-  sed -i "s|src/plugins/${plugin}.cpp|#&|g" src/mavros/mavros/CMakeLists.txt; \
-fi; \
-done && \
-for extra in $ALL_EXTRAS; do \
-  if [[ $ENABLED_EXTRAS != *"$extra"* ]]; then \
-    sed -i "s|src/plugins/${extra}.cpp|#&|g" src/mavros/mavros_extras/CMakeLists.txt; \
-  fi; \
-done && \
-for msg in $ALL_MSGS; do \
-if [[ $ENABLED_MSGS != *"$msg"* ]]; then \
-  sed -i "s|msg/${msg}.msg|#&|g" src/mavros/mavros_msgs/CMakeLists.txt; \
-fi; \
-done && \
-for srv in $ALL_SRVS; do \
-  if [[ $ENABLED_SRVS != *"$srv"* ]]; then \
-    sed -i "s|srv/${srv}.srv|#&|g" src/mavros/mavros_msgs/CMakeLists.txt; \
-  fi; \
-done
+# Use awk to replace the original plugin/message/service lists with our new ones
+awk -v r="$(build_new_list ENABLED_PLUGINS src/plugins cpp)" '/add_library\(mavros_plugins SHARED/ {print; print r; f=1} /# \[\[\[end\]\]\] \(checksum: ccf56c1a56e9dccf8464483f7b1eab99\)/ {f=0} !f' $MAVROS_PATH/mavros/CMakeLists.txt > tmp && mv tmp $MAVROS_PATH/mavros/CMakeLists.txt
+awk -v r="$(build_new_list ENABLED_EXTRAS src/plugins cpp)" '/add_library\(mavros_extras_plugins SHARED/ {print; print r; f=1} /# \[\[\[end\]\]\] \(checksum: a6dc36e6871deb96f56d61e2a1a38f37\)/ {f=0} !f' $MAVROS_PATH/mavros_extras/CMakeLists.txt > tmp && mv tmp $MAVROS_PATH/mavros_extras/CMakeLists.txt
+awk -v r="$(build_new_list ENABLED_MSGS msg msg)" '/set\(msg_files/ {print; print r; f=1} /# \[\[\[end\]\]\] \(checksum: cf55cc727ab8f7a2823267cab0dd1bf3\)/ {f=0} !f' $MAVROS_PATH/mavros_msgs/CMakeLists.txt > tmp && mv tmp $MAVROS_PATH/mavros_msgs/CMakeLists.txt
+awk -v r="$(build_new_list ENABLED_SRVS srv srv)" '/set\(srv_files/ {print; print r; f=1} /# \[\[\[end\]\]\] \(checksum: a011691dec330a2a89d35288ebf05c55\)/ {f=0} !f' $MAVROS_PATH/mavros_msgs/CMakeLists.txt > tmp && mv tmp $MAVROS_PATH/mavros_msgs/CMakeLists.txt

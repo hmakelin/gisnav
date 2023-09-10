489a7f02a8d4c08f3ed17ec49cbba202613585d6
Harri Mäkelin <hmakelin@protonmail.com>
2023-09-10 10:47:12 +0100

    Refactor loftr_handler

--- docker/torch-serve/loftr_handler.py
+++ docker/torch-serve/loftr_handler.py
@@ -8,135 +8,152 @@ from ts.torch_handler.base_handler import BaseHandler
 
 
 def bgr2gray(bgr):
-    """Convert (OpenCV) BGR format to grayscale"""
-    b, g, r = bgr[:, :, 0], bgr[:, :, 1], bgr[:, :, 2]
-    gray = 0.114 * b + 0.587 * g + 0.299 * r
-    return gray
+    """Converts BGR format image to grayscale."""
+    return cv2.cvtColor(bgr, cv2.COLOR_BGR2GRAY)
 
 
 class LoFTRHandler(BaseHandler):
     MIN_MATCHES = 15
-    """Minimum amount of keypoint matches for a good match"""
-
     CONFIDENCE_THRESHOLD = 0.7
-    """Confidence threshold for filtering out bad matches"""
 
     def _load_pickled_model(self, _, model_file, model_pt_path):
-        """Recognize LoFTR model definition inside loftr.py and pass config"""
+        """Loads the :term:`LoFTR` model"""
         module = importlib.import_module(model_file.split(".")[0])
-        model_ = getattr(module, "LoFTR")
-        model = model_()
+        model_cls = getattr(module, "LoFTR")
+        model = model_cls()
         state_dict = torch.load(model_pt_path, map_location=self.device)
         model.load_state_dict(state_dict)
         return model
 
     def preprocess(self, data):
-        """Convert incoming images to torch tensors"""
+        """Converts incoming images to torch tensors"""
         data = data[0]
-        qry = pickle.loads(data["query"])
-        ref = pickle.loads(data["reference"])
-        k = pickle.loads(data["k"])
-        elevation = pickle.loads(data["elevation"])
-        qry_grayscale = bgr2gray(qry)
-        ref_grayscale = bgr2gray(ref)
-        qry_tensor = torch.Tensor(qry_grayscale)[None][None].cuda() / 255.0
-        ref_tensor = torch.Tensor(ref_grayscale)[None][None].cuda() / 255.0
-        batch = {"image0": qry_tensor, "image1": ref_tensor}
-        return batch, qry, ref, k, elevation
+        query_img, reference_img, k_matrix, elevation = self._deserialize_data(data)
+        self._display_images("Query", query_img, "Reference", reference_img)
+
+        qry_tensor, ref_tensor = self._convert_images_to_tensors(
+            query_img, reference_img
+        )
+
+        return (
+            {"image0": qry_tensor, "image1": ref_tensor},
+            query_img,
+            reference_img,
+            k_matrix,
+            elevation,
+        )
+
+    @staticmethod
+    def _deserialize_data(data):
+        """Deserializes pickled data"""
+        return (
+            pickle.loads(data[key]) for key in ["query", "reference", "k", "elevation"]
+        )
+
+    @staticmethod
+    def _display_images(*args):
+        """Displays images using OpenCV"""
+        for i in range(0, len(args), 2):
+            cv2.imshow(args[i], args[i + 1])
+        cv2.waitKey(1)
+
+    @staticmethod
+    def _convert_images_to_tensors(qry, ref):
+        """Converts grayscale images to torch tensors"""
+        qry_tensor = torch.Tensor(bgr2gray(qry)[None, None]).cuda() / 255.0
+        ref_tensor = torch.Tensor(bgr2gray(ref)[None, None]).cuda() / 255.0
+        return qry_tensor, ref_tensor
 
     def inference(self, preprocessed_data):
-        """Do keypoint matching"""
-        batch, qry, ref, k, elevation = preprocessed_data
+        """Do keypoint matching."""
         with torch.no_grad():
-            results = self.model(batch)
-        return results, qry, ref, k, elevation
+            results = self.model(preprocessed_data[0])
+        return results, *preprocessed_data[1:]
 
     def postprocess(self, inferred_data):
-        """Filter based on confidence threshold"""
-        results, qry, ref, k, elevation = inferred_data
-        mkp_qry = results["keypoints0"].cpu().numpy()
-        mkp_ref = results["keypoints1"].cpu().numpy()
+        """Filters matches based on confidence threshold and calculates :term:`pose`"""
+        results, query_img, reference_img, k_matrix, elevation = inferred_data
+        mkp_qry, mkp_ref = self._filter_matches_based_on_confidence(results)
+
+        if not mkp_qry or len(mkp_qry) < self.MIN_MATCHES:
+            return [{}]
+
+        mkp2_3d = self._compute_3d_points(mkp_ref, elevation)
+        pose = self._compute_pose(mkp2_3d, mkp_qry, k_matrix)
+        self._visualize_matches_and_pose(
+            query_img, reference_img, mkp_qry, mkp_ref, k_matrix, *pose
+        )
+
+        return [{"r": pose[0].tolist(), "t": pose[1].tolist()}]
+
+    def _filter_matches_based_on_confidence(self, results):
+        """Filters matches based on confidence threshold"""
         conf = results["confidence"].cpu().numpy()
         valid = conf > self.CONFIDENCE_THRESHOLD
+        return (
+            results["keypoints0"].cpu().numpy()[valid, :],
+            results["keypoints1"].cpu().numpy()[valid, :],
+        )
 
-        mkp_qry = mkp_qry[valid, :]
-        mkp_ref = mkp_ref[valid, :]
-
-        print(f"{np.amax(mkp_ref, axis=0)} {np.amax(mkp_ref, axis=1)}")
-
-        if mkp_qry is None or len(mkp_qry) < self.MIN_MATCHES:
-            return [{}]  # Not enough matching keypoints found
-
+    @staticmethod
+    def _compute_3d_points(mkp_ref, elevation):
+        """Computes 3D points from matches"""
         if elevation is None:
-            # Set world z values to zero
-            # assume planar ground surface and estimate homography
-            z_values = np.array([[0]] * len(mkp_qry))
-        else:
-            # Z values assumed to be in camera native coordinates (not meters)
-            x, y = np.transpose(
-                np.floor(mkp_ref).astype(int)
-            )  # do floor first before converting to int
-            z_values = elevation[y, x].reshape(-1, 1)  # y axis first  # TODO: correct?
+            return np.hstack((mkp_ref, np.zeros((len(mkp_ref), 1))))
 
-        mkp2_3d = np.hstack((mkp_ref, z_values))
+        x, y = np.transpose(np.floor(mkp_ref).astype(int))
+        z_values = elevation[y, x].reshape(-1, 1)
+        return np.hstack((mkp_ref, z_values))
 
+    @staticmethod
+    def _compute_pose(mkp2_3d, mkp_qry, k_matrix):
+        """Computes :term:`pose` using :func:`cv2.solvePnPRansac`"""
         dist_coeffs = np.zeros((4, 1))
-        _, r, t, __ = cv2.solvePnPRansac(
+        _, r, t, _ = cv2.solvePnPRansac(
             mkp2_3d,
             mkp_qry,
-            k,
+            k_matrix,
             dist_coeffs,
-            None,
-            None,
             useExtrinsicGuess=False,
             iterationsCount=10,
         )
-        r, _ = cv2.Rodrigues(r)
+        r_matrix, _ = cv2.Rodrigues(r)
+        return r_matrix, t
 
-        self.visualize(qry, ref, mkp_qry, mkp_ref, k, r, t)
+    def _visualize_matches_and_pose(self, qry, ref, mkp_qry, mkp_ref, k, r, t):
+        """Visualizes matches and projected :term:`FOV`"""
+        h_matrix = k @ np.delete(np.hstack((r, t)), 2, 1)
+        projected_fov = self._project_fov(qry, h_matrix)
+        img_with_fov = cv2.polylines(
+            ref, [np.int32(projected_fov)], True, 255, 3, cv2.LINE_AA
+        )
 
-        return [{"r": r.tolist(), "t": t.tolist()}]
+        mkp_qry = [cv2.KeyPoint(x[0], x[1], 1) for x in mkp_qry]
+        mkp_ref = [cv2.KeyPoint(x[0], x[1], 1) for x in mkp_ref]
+        matches = [cv2.DMatch(i, i, 0) for i in range(len(mkp_qry))]
+
+        match_img = cv2.drawMatches(
+            img_with_fov,
+            mkp_ref,
+            qry,
+            mkp_qry,
+            matches,
+            None,
+            matchColor=(0, 255, 0),
+            flags=2,
+        )
+
+        cv2.imshow("Matches and field of view", match_img)
+        cv2.waitKey(1)
 
     @staticmethod
-    def _make_keypoint(pt, sz=1.0):
-        """Converts tuple to a cv2.KeyPoint."""
-        return cv2.KeyPoint(pt[0], pt[1], sz)
-
-    def visualize(self, qry, ref, mkp_qry, mkp_ref, k, r, t):
-        print(mkp_qry.shape)
-
-        # Visualize matches and projected FOV
-        h = k @ np.delete(np.hstack((r, t)), 2, 1)
-        height, width = qry.shape[0:2][::-1]  # cv2 flips axis order
+    def _project_fov(img, h_matrix):
+        """Projects :term:`FOV` on :term:`reference` image"""
+        height, width = img.shape[0:2]
         src_pts = np.float32(
-            [[0, 0], [height - 1, 0], [height - 1, width - 1], [0, width - 1]]
+            [[0, 0], [width - 1, 0], [width - 1, height - 1], [0, height - 1]]
         ).reshape(-1, 1, 2)
         try:
-            fov_pix = cv2.perspectiveTransform(src_pts, np.linalg.inv(h))
-            map_with_fov = cv2.polylines(
-                ref,
-                [np.int32(fov_pix)],
-                True,
-                255,
-                3,
-                cv2.LINE_AA,
-            )
+            return cv2.perspectiveTransform(src_pts, np.linalg.inv(h_matrix))
         except np.linalg.LinAlgError:
-            # cannot project FOV
-            map_with_fov = ref
-
-        draw_params = dict(
-            matchColor=(0, 255, 0), singlePointColor=None, matchesMask=None, flags=2
-        )
-
-        # Need cv2.KeyPoints and cv2.DMatches to make cv2.drawMatches work
-        mkp_ref = np.apply_along_axis(self._make_keypoint, 1, mkp_ref)
-        mkp_qry = np.apply_along_axis(self._make_keypoint, 1, mkp_qry)
-        matches = list(map(lambda i: cv2.DMatch(i, i, 0), range(0, len(mkp_qry))))
-
-        img = cv2.drawMatches(
-            map_with_fov, mkp_ref, qry, mkp_qry, matches, None, **draw_params
-        )
-
-        cv2.imshow("Matches and field of view", img)
-        cv2.waitKey(1)
+            return src_pts

WIP: fix camera quaternion

gisnav/test/_mocks.py
@@ -6,10 +6,13 @@ and are therefore kept in a separate module.
 This module also defines default values for a starting global position and
 orientation that are appropriate for the :term:`KSQL` airport simulation.
 """
-import numpy as np
+import numpy as
+import tf2_ros
+import tf.transformations as tf_transform
 from geometry_msgs.msg import PoseStamped, Quaternion
 from mavros_msgs.msg import GimbalDeviceAttitudeStatus
 from sensor_msgs.msg import NavSatFix
+from geographic_msgs.msg import GeoPoseStamped
 
 from gisnav.messaging import create_header
 
@@ -34,6 +37,12 @@ VEHICLE_ELLIPSOID_ALTITUDE_METERS = 100.0
 VEHICLE_ENU_QUATERNION = np.array([0.0, 0.0, 0.0, 1.0])
 """Default mock :term:`vehicle` :term:`KSQL` airport starting location
 :term:`orientation` quaternion in :term:`ENU` frame in (x, y, z, w) format.
+"""
+
+
+VEHICLE_NED_QUATERNION = np.array([0.0, 0.0, 0.0, 1.0])
+"""Default mock :term:`vehicle` :term:`KSQL` airport starting location
+:term:`orientation` quaternion in :term:`NED` frame in (x, y, z, w) format.
 """
 
 
@@ -64,6 +73,8 @@ def navsatfix(
     :return: A :class:`sensor_msgs.msg.NavSatFix` message representing
         the vehicle's :term:`global position`
     """
+    quaternion = tf_transform.quaternion_from_euler(roll, pitch, yaw)
+
     navsatfix_msg = NavSatFix()
     navsatfix_msg.header = create_header("base_link")
     navsatfix_msg.latitude = vehicle_lat_degrees
@@ -114,3 +125,31 @@ def gimbal_device_attitude_status(
         w=camera_frd_quaternion[3],
     )
     return attitude
+
+
+def vehicle_geopose(
+    vehicle_lat_degrees: float = VEHICLE_LATITUDE_DEGREES,
+    vehicle_lon_degrees: float = VEHICLE_LONGITUDE_DEGREES,
+    vehicle_alt_ellipsoid_meters: float = VEHICLE_ELLIPSOID_ALTITUDE_METERS,
+    vehicle_ned_quaternion: np.ndarray = VEHICLE_NED_QUATERNION,
+) -> PoseStamped:
+    """Returns a mock :class:`geographic_msgs.msg.GeoPoseStamped` :term:`ROS` message
+    based on given :term:`vehicle` :term:`WGS 84` latitude and longitude
+    coordinates and :term:`ellipsoid` altitude in meters and :term:`NED` quaternion.
+
+    :param vehicle_lat_degrees: Vehicle WGS 84 latitude coordinate in degrees
+    :param vehicle_lon_degrees: Vehicle WGS 84 longitude coordinate in degrees
+    :param vehicle_alt_ellipsoid_meters: Vehicle ellipsoid altitude in meters
+    :param vehicle_ned_quaternion: Vehicle NED quaternion in (x, y, z, w) format
+    :return: A :class:`geographic_msgs.msg.GeoPoseStamped` message representing
+        the vehicle's :term:`global position` and :term:`orientation`
+    """
+    geopose = GeoPoseStamped()
+    geopose.pose.position.latitude = vehicle_lat_degrees
+    geopose.pose.position.longitude = vehicle_lon_degrees
+    geopose.pose.position.altitude = vehicle_alt_ellipsoid_meters
+    geopose.pose.orientation.x = vehicle_ned_quaternion[0]
+    geopose.pose.orientation.y = vehicle_ned_quaternion[1]
+    geopose.pose.orientation.z = vehicle_ned_quaternion[2]
+    geopose.pose.orientation.w = vehicle_ned_quaternion[3]
+    return geopose

gisnav/test/unit/test_gisnode.py
@@ -10,22 +10,32 @@ from scipy.spatial.transform import Rotation
 from gisnav.core import GISNode
 
 
+def _generate_random_quaternions(n):
+    quaternions = []
+    for _ in range(n):
+        rand_nums = np.random.randn(4)
+        norm = np.linalg.norm(rand_nums)
+        quaternion = rand_nums / norm  # Normalize to have a magnitude of 1
+        quaternions.append(quaternion)
+    return np.array(quaternions)
+
+
 class TestVehicleGeoPoseCase(unittest.TestCase):
     """Unit tests the :attr:`.GISNode.vehicle_geopose` property"""
 
     def __init__(self, *args, **kwargs):
         """Initializes the test case"""
         super().__init__(*args, **kwargs)
+        self.context: Optional[rclpy.Context] = None
 
-    @classmethod
-    def setUpClass(cls):
+    def setUp(self):
         """Initializes rclpy context"""
-        rclpy.init()
+        self.context = rclpy.Context()
+        rclpy.init(context=self.context)
 
-    @classmethod
-    def tearDownClass(cls):
+    def tearDown(self):
         """Shuts down rclpy context"""
-        rclpy.shutdown()
+        rclpy.shutdown(context=self.context)
 
     @staticmethod
     def _enu_to_ned_quaternion(enu_quat: np.ndarray):
@@ -51,21 +61,11 @@ class TestVehicleGeoPoseCase(unittest.TestCase):
         attitude_ned = Rotation.from_euler("XYZ", rpy, degrees=True)
         return attitude_ned.as_quat()
 
-    @staticmethod
-    def _generate_random_quaternions(n):
-        quaternions = []
-        for _ in range(n):
-            rand_nums = np.random.randn(4)
-            norm = np.linalg.norm(rand_nums)
-            quaternion = rand_nums / norm  # Normalize to have a magnitude of 1
-            quaternions.append(quaternion)
-        return np.array(quaternions)
-
     @patch.object(GISNode, "vehicle_pose", mocks.vehicle_pose())
     def test_valid_navsatfix_input(self):
         """Tests that :attr:`.GISNode.vehicle_geopose` is correctly computed
         given various valid :attr:`.GISNode.nav_sat_fix` values when a valid
-        for :attr:`.GISNode.vehicle_pose` is available.
+        :attr:`.GISNode.vehicle_pose` is available.
         """
         with patch(
             "gisnav.core.GISNode.nav_sat_fix", new_callable=PropertyMock
@@ -73,7 +73,7 @@ class TestVehicleGeoPoseCase(unittest.TestCase):
             mock_navsatfix.return_value = mocks.navsatfix()
             # Create an instance of GISNode whose vehicle_geopose property is
             # under test.
-            gis_node = GISNode("gis_node")
+            gis_node = GISNode("gis_node", context=self.context)
             pose = gis_node.vehicle_geopose.pose
             self.assertEqual(pose.position.latitude, mocks.VEHICLE_LATITUDE_DEGREES)
             self.assertEqual(pose.position.longitude, mocks.VEHICLE_LONGITUDE_DEGREES)
@@ -85,16 +85,16 @@ class TestVehicleGeoPoseCase(unittest.TestCase):
     def test_valid_pose_input(self):
         """Tests that :attr:`.GISNode.vehicle_geopose` is correctly computed
         given various valid :attr:`.GISNode.vehicle_pose` values when a valid
-        for :attr:`.GISNode.nav_sat_fix` is available.
+        :attr:`.GISNode.nav_sat_fix` is available.
         """
         with patch(
             "gisnav.core.GISNode.vehicle_pose", new_callable=PropertyMock
         ) as mock_vehicle_pose:
-            for q in self._generate_random_quaternions(10):
+            for q in _generate_random_quaternions(10):
                 mock_vehicle_pose.return_value = mocks.vehicle_pose(q)
                 # Create an instance of GISNode whose vehicle_geopose property is
                 # under test.
-                gis_node = GISNode("gis_node")
+                gis_node = GISNode("gis_node", context=self.context)
                 pose = gis_node.vehicle_geopose.pose
 
                 expected_ned_quaternion = self._enu_to_ned_quaternion(q)
@@ -129,6 +129,57 @@ class TestVehicleGeoPoseCase(unittest.TestCase):
     def test_global_position_edge_cases(self):
         raise NotImplementedError
 
+
+class TestCameraQuaternionCase(unittest.TestCase):
+    """Unit tests the :attr:`.GISNode.camera_quaternion` property"""
+
+    def __init__(self, *args, **kwargs):
+        """Initializes the test case"""
+        super().__init__(*args, **kwargs)
+        self.context: Optional[rclpy.Context] = None
+
+    def setUp(self):
+        """Initializes rclpy context"""
+        self.context = rclpy.Context()
+        rclpy.init(context=self.context)
+
+    def tearDown(self):
+        """Shuts down rclpy context"""
+        rclpy.shutdown(context=self.context)
+
+    @patch.object(GISNode, "gimbal_device_attitude_status", mocks.gimbal_device_attitude_status())
+    def test_valid_vehicle_geopose_input(self):
+        """Tests that :attr:`.GISNode.camera_quaternion` is correctly computed
+        given various valid :attr:`.GISNode.vehicle_geopose` values when a valid
+        :attr:`.GISNode.gimbal_device_attitude_status` is available.
+        """
+        with patch(
+            "gisnav.core.GISNode.vehicle_pose", new_callable=PropertyMock
+        ) as mock_vehicle_pose:
+            for q in _generate_random_quaternions(10):
+                mock_vehicle_pose.return_value = mocks.vehicle_pose(q)
+                # Create an instance of GISNode whose vehicle_geopose property is
+                # under test.
+                gis_node = GISNode("gis_node", context=self.context)
+                quaternion = gis_node.camera_quaternion.quaternion
+                computed_orientation = np.array(
+                    [
+                        quaternion.x,
+                        quaternion.y,
+                        quaternion.z,
+                        quaternion.w,
+                    ]
+                )
+                np.testing.assert_array_almost_equal(
+                    computed_orientation, mocks.CAMERA_FRD_QUATERNION, decimal=5
+                )
+
+    @patch.object(GISNode, "vehicle_geopose", mocks.vehicle_geopose())
+    def test_valid_gimbal_device_attitude_status_input(self):
+        """Tests that :attr:`.GISNode.camera_quaternion` is correctly computed
+        given various valid :attr:`.GISNode.gimbal_device_attitude_status`
+        values when a valid :attr:`.GISNode.vehicle_geopose` is available.
+        """
 
 if __name__ == "__main__":
     unittest.main()
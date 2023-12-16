9e6e9307fd2c6d65584665bed424adedca1f8483
Harri Makelin <hmakelin@protonmail.com>
2023-12-16 11:04:24 +0000

    Remove unused private method

--- gisnav/gisnav/extensions/mock_gps_node.py
+++ gisnav/gisnav/extensions/mock_gps_node.py
@@ -198,13 +198,6 @@ class MockGPSNode(Node):
         Uses the release/1.14 tag version of :class:`px4_msgs.msg.SensorGps`
         """
 
-        def _dot_product(vector1: Vector3, vector2: Vector3) -> Vector3:
-            return Vector3(
-                x=vector1.x * vector2.x,
-                y=vector1.y * vector2.y,
-                z=vector1.z * vector2.z
-            )
-
         @narrow_types(self)
         def _sensor_gps(
             camera_to_reference: TransformStamped,

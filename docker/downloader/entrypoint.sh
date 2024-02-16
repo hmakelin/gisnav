193795bd8e158c0d2f092e030c884f08f5dd3fcf
Harri Mäkelin <hmakelin@protonmail.com>
2024-02-16 22:59:05 +0000

    Update inotify script

--- docker/downloader/entrypoint.sh
+++ docker/downloader/entrypoint.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 set -e
-DOWNLOAD_DIR="/etc/mapserver"
 
-# watch for new files in the mapped volume and unzip automatically
+DOWNLOAD_DIR="/etc/mapserver"
+TARGET_DIR="/etc/mapserver/imagery"
+
+# watch for new files in the download dir
 inotifywait -m -e create -e moved_to --format '%f' "${DOWNLOAD_DIR}" | while read FILENAME
 do
     if [[ "${FILENAME}" =~ \.zip$ ]]; then
-        unzip -o "${DOWNLOAD_DIR}/${FILENAME}" -d "${DOWNLOAD_DIR}"
+        # unzip automatically into target dir
+        mkdir -p "${TARGET_DIR}"
+        unzip -o "${DOWNLOAD_DIR}/${FILENAME}" -d "${TARGET_DIR}"
         # Remove the zip file after extraction
         rm "${DOWNLOAD_DIR}/${FILENAME}"
     fi
+    # rebuild .VRT file used for imagery layer
+    # todo: gdalbuildvrt
 done
 
 exec "$@"
-

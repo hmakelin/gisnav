f2249469c85166021ba481c893393da2d9a7eb5d
Harri Makelin <hmakelin@protonmail.com>
2024-02-26 20:14:14 +0000

    Ensure fileserver can edit files on shared volume [skip ci]

--- docker/apache/fileserver/entrypoint.sh
+++ docker/apache/fileserver/entrypoint.sh
@@ -7,4 +7,8 @@ rm -f /var/run/apache2/cgisock.*
 
 apache2ctl -D FOREGROUND &
 
+# Make sure we can edit files on mapped volumes
+chown -R www-data:www-data /var/www/filegator/repository
+chmod -R 775  /var/www/filegator/repository
+
 exec "$@"

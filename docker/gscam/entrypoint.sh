2ab198ed3a0f887209029997f86069effd088bf1
Harri Makelin <hmakelin@protonmail.com>
2024-02-28 18:27:59 +0000

    Fix entrypoint file move exception handling

--- docker/gscam/entrypoint.sh
+++ docker/gscam/entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 source "/opt/ros/$ROS_VERSION/setup.bash"
 
 # Move config files to shared volume if not yet done
-mv /gscam_params.yaml /etc/gscam | echo "INFO: gscam_params.yaml not found on container - likely already moved to shared volume"
-mv /camera_calibration.yaml /etc/gscam | echo "INFO: camera_calibration.yaml on container - likely already moved to shared volume"
+mv /gscam_params.yaml /etc/gscam || echo "INFO: gscam_params.yaml not found on container - likely already moved to shared volume"
+mv /camera_calibration.yaml /etc/gscam || echo "INFO: camera_calibration.yaml on container - likely already moved to shared volume"
 
 exec "$@"

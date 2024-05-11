acfa9b503ed9900e915a2abe32260cc320e8f98c
Harri Makelin <hmakelin@protonmail.com>
2024-05-11 09:01:55 +0100

    Update test subpackage docstrings [skip ci]

--- gisnav/test/launch/__init__.py
+++ gisnav/test/launch/__init__.py
@@ -1,7 +1,1 @@
-"""Launch tests
-
-.. note::
-
-    This subpackage is still very much Work In Progress - do not expect the tests
-    here to work.
-"""
+"""This sub-package contains launch tests"""

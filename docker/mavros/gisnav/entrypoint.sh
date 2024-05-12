b2c340cc0ff7425d0135cf59a99dc06727e1de9e
Harri Makelin <hmakelin@protonmail.com>
2024-05-12 10:52:28 +0100

    Enable npm to build docs in gisnav container

--- docker/mavros/gisnav/entrypoint.sh
+++ docker/mavros/gisnav/entrypoint.sh
@@ -4,6 +4,9 @@ set -e
 source "/opt/ros/$ROS_VERSION/setup.bash"
 source "/opt/colcon_ws/install/setup.bash" --
 
+# Ensure we have npm to build docs
+source ~/.nvm/nvm.sh
+
 # Needed for pip installed dev tools like pre-commit and sphinx-build
 export PATH=/usr/lib/gisnav:$PATH
 

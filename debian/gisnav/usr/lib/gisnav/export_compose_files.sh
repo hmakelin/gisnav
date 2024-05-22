8ae5849b40c296623adee3f17252f5b8d8a66cad
Harri Makelin <hmakelin@protonmail.com>
2024-05-22 16:31:47 +0100

    Use new docker compose overrides in gnv executable

--- debian/gisnav/usr/lib/gisnav/export_compose_files.sh
+++ debian/gisnav/usr/lib/gisnav/export_compose_files.sh
@@ -6,22 +6,33 @@
 
 gisnav_docker_home=/etc/gisnav/docker
 
+# Base compose files, excluding GPU-specific ones
+compose_files="-f $gisnav_docker_home/docker-compose.yaml \
+               -f $gisnav_docker_home/docker-compose.commands.yaml \
+               -f $gisnav_docker_home/docker-compose.dependencies.yaml \
+               -f $gisnav_docker_home/docker-compose.labels.yaml \
+               -f $gisnav_docker_home/docker-compose.networking.yaml \
+               -f $gisnav_docker_home/docker-compose.ros.yaml \
+               -f $gisnav_docker_home/docker-compose.socket.yaml \
+               -f $gisnav_docker_home/docker-compose.tty.yaml \
+               -f $gisnav_docker_home/docker-compose.volumes.yaml \
+               -f $gisnav_docker_home/docker-compose.x11.yaml"
+
+# Determine the GPU override
 case $GISNAV_GPU_TYPE in
     "nvidia")
         echo "Using Docker Compose with Nvidia override."
-        compose_files="-f $gisnav_docker_home/docker-compose.yaml -f $gisnav_docker_home/docker-compose.nvidia.yaml"
+        compose_files="$compose_files -f $gisnav_docker_home/docker-compose.gpu.nvidia.yaml"
         ;;
     "broadcom")
         echo "Using Docker Compose with Broadcom override."
-        compose_files="-f $gisnav_docker_home/docker-compose.yaml -f $gisnav_docker_home/docker-compose.broadcom.yaml"
+        compose_files="$compose_files -f $gisnav_docker_home/docker-compose.gpu.broadcom.yaml"
         ;;
     "none")
         echo "No GPU detected. Using Docker Compose without GPU-specific overrides."
-        compose_files="-f $gisnav_docker_home/docker-compose.yaml"
         ;;
     *)
         echo "Unknown GPU type detected. Using Docker Compose without GPU-specific overrides."
-        compose_files="-f $gisnav_docker_home/docker-compose.yaml"
         ;;
 esac
 

59fbe59fbf657adfff1694f67fe69805bc88bfbb
Harri Makelin <hmakelin@protonmail.com>
2024-06-02 07:26:44 +0100

    Eliminate unzip attempts on deleted zipfiles

--- docker/apache/mapserver/entrypoint.sh
+++ docker/apache/mapserver/entrypoint.sh
@@ -11,6 +11,9 @@ DEM_DIR="/etc/mapserver/maps/dem"
 IMAGERY_VRT_FILE="imagery.vrt"
 DEM_VRT_FILE="dem.vrt"
 
+# GDAL supported raster formats
+SUPPORTED_FORMATS=("tif" "tiff" "jp2" "ecw" "img")
+
 mkdir -p $IMAGERY_DIR
 mkdir -p $DEM_DIR
 
@@ -66,47 +69,87 @@ generate_vrt() {
     DIR_PATH=$1
     VRT_FILE=$2
 
-    echo "Generating VRT file: $VRT_FILE"
-    cd /etc/mapserver && gdalbuildvrt "$VRT_FILE" "$DIR_PATH"/*.tif "$DIR_PATH"/*.tiff "$DIR_PATH"/*.jp2 "$DIR_PATH"/*.ecw "$DIR_PATH"/*.img
+    # Initialize an empty array to hold the file paths
+    FILE_LIST=()
+
+    # Add file paths to the list if they exist
+    for ext in "${SUPPORTED_FORMATS[@]}"; do
+        files=("$DIR_PATH"/*.$ext)
+        if [ -e "${files[0]}" ]; then
+            echo $ext found
+            FILE_LIST+=("${files[@]}")
+        fi
+    done
+
+    # Check if any supported files were found
+    if [ ${#FILE_LIST[@]} -gt 0 ]; then
+        echo "Generating VRT file: $VRT_FILE"
+        cd /etc/mapserver && gdalbuildvrt "$VRT_FILE" "${FILE_LIST[@]}"
+    else
+        echo "No supported files found in $DIR_PATH to generate VRT"
+    fi
 }
 
 process_directory_change() {
     DIR_PATH=$1
     VRT_FILE=$2
     FILE=$3
+    EVENT=$4
 
-    echo "Detected change in file: $FILE"
+    echo "Detected $EVENT event for file: $FILE"
     BASENAME=$(basename "$FILE")
+
+    # Convert EVENT to lowercase
+    EVENT=$(echo "$EVENT" | tr '[:upper:]' '[:lower:]')
 
     # Skip temporary download files
     if [[ "$BASENAME" =~ \.part$|\.download$ ]]; then
         echo "Ignoring partial download file: $FILE"
     elif [[ "$BASENAME" =~ \.zip$ ]]; then
-        echo "Scheduling unzip of $FILE..."
-        unzip_with_retry $FILE $DIR_PATH
-    elif [[ "$BASENAME" =~ \.(tif|tiff|jp2|ecw|img)$ ]]; then
-        echo "Raster file detected, regenerating VRT."
-        generate_vrt $DIR_PATH $VRT_FILE
+        if [[ "$EVENT" != "delete" ]]; then
+            echo "Scheduling unzip of $FILE..."
+            unzip_with_retry $FILE $DIR_PATH
+        fi
+    else
+        # Check if the file matches any of the supported formats
+        for ext in "${SUPPORTED_FORMATS[@]}"; do
+            if [[ "$BASENAME" =~ \.$ext$ ]]; then
+                echo "Raster file detected, regenerating VRT."
+                generate_vrt $DIR_PATH $VRT_FILE
+                break
+            fi
+        done
     fi
     return
 }
 
 # Monitor IMAGERY_DIR
-inotifywait -m -e create -e moved_to -e delete "$IMAGERY_DIR" --format '%w%f' | while read FILE
+inotifywait -m -e create -e moved_to -e delete "$IMAGERY_DIR" --format '%e %w%f' | while read EVENT FILE
 do
-    process_directory_change "$IMAGERY_DIR" "$IMAGERY_VRT_FILE" "$FILE"
+    process_directory_change "$IMAGERY_DIR" "$IMAGERY_VRT_FILE" "$FILE" "$EVENT"
 done &
 
 # Monitor DEM_DIR
-inotifywait -m -e create -e moved_to -e delete "$DEM_DIR" --format '%w%f' | while read FILE
+inotifywait -m -e create -e moved_to -e delete "$DEM_DIR" --format '%e %w%f' | while read EVENT FILE
 do
-    process_directory_change "$DEM_DIR" "$DEM_VRT_FILE" "$FILE"
+    process_directory_change "$DEM_DIR" "$DEM_VRT_FILE" "$FILE" "$EVENT"
 done &
 
 # Move demo imagery and DEM over to shared volume if still on image
 # This should trigger the inotify script
-mv /etc/mapserver/$NAIP_ZIP_FILENAME $IMAGERY_DIR || echo "NAIP imagery not found on container - likely already moved to shared volume"
-mv /etc/mapserver/$DEM_FILENAME $DEM_DIR || echo "USGS DEM not found on container - likely already moved to shared volume"
+# Check and move NAIP imagery if it exists
+if [ -f /etc/mapserver/$NAIP_ZIP_FILENAME ]; then
+    mv /etc/mapserver/$NAIP_ZIP_FILENAME $IMAGERY_DIR
+else
+    echo "NAIP imagery not found on container - likely already moved to shared volume"
+fi
+
+# Check and move USGS DEM if it exists
+if [ -f /etc/mapserver/$DEM_FILENAME ]; then
+    mv /etc/mapserver/$DEM_FILENAME $DEM_DIR
+else
+    echo "USGS DEM not found on container - likely already moved to shared volume"
+fi
 
 # Regenerate .vrt files on startup in case they have been destroyed for some reason
 generate_vrt "$DEM_DIR" "$DEM_VRT_FILE"

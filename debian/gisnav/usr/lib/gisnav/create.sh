29cedfd07b5e5e8a8d04b147e398ed267c88d764
Harri Makelin <hmakelin@protonmail.com>
2024-06-07 09:10:03 +0100

    Only run expose-xhost in gnc create if X display server is availabl

--- debian/gisnav/usr/lib/gisnav/create.sh
+++ debian/gisnav/usr/lib/gisnav/create.sh
@@ -6,5 +6,7 @@ gisnav_docker_home=/etc/gisnav/docker
 
 /usr/lib/gisnav/compose.sh "$@"
 
-# Expose X server to new containers
-make -C $gisnav_docker_home expose-xhost
+# Expose X server to new containers if X display server is available
+if xdpyinfo >/dev/null 2>&1; then
+    make -C $gisnav_docker_home expose-xhost
+fi
